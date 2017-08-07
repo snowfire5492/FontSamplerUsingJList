ec63b1280a4f67ed07292850746ce0c7314480a7
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2017-08-07 10:10:10 -0700

    update code

---
+++ GenerateFontListInJava.java
@@ -0,0 +1,18 @@
+import java.awt.GraphicsEnvironment;
+
+public class GenerateFontListInJava
+{
+
+  public static void main(String[] args)
+  {
+    String fonts[] = 
+      
+GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+
+    for ( int i = 0; i < fonts.length; i++ )
+    {
+      System.out.println(fonts[i]);
+    }
+  }
+
+}
