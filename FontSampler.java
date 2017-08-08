ff65de0a371dc6b3956ee6824a1c131c0acebf63
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2017-08-08 10:10:10 -0700

    Font Sampler code

---
+++ FontSampler.java
@@ -0,0 +1,20 @@
+  GNU nano 3.2                    FontSampler.java                    
+Modified
+
+displayText.getFont().getSize()));
+
+                });
+        }
+
+        public static void main(String[] args){
+                SwingUtilities.invokeLater(() ->
+                new FontSampler());
+        }
+
+}
+
+
+
+
+
+
